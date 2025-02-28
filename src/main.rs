4debf91b72077d8a7e5d14a6c5562c3d18ae2746
Toka <tokazerkje@outlook.com>
2025-02-28 18:33:57 +0100

    upd

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
-use std::path::{Path, PathBuf};
-use std::ffi::OsStr;
 use std::env;
+use std::ffi::OsStr;
+use std::fs::{remove_file, symlink_metadata};
 use std::os::unix::fs;
+use std::path::{Path, PathBuf};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -10,7 +11,7 @@ fn main() {
         return;
     }
     let version = format!("-{}", args[1]);
-    
+
     let llvm_files = glob::glob("/usr/bin/llvm*").unwrap();
     let clang_files = glob::glob("/usr/bin/clang*").unwrap();
     let specific_files = vec![
@@ -19,14 +20,31 @@ fn main() {
         format!("/usr/bin/lld{}", version),
     ];
 
-    for entry in llvm_files.chain(clang_files).chain(specific_files.iter().map(|s| Ok(PathBuf::from(s.to_string())))) {
+    for entry in llvm_files.chain(clang_files).chain(
+        specific_files
+            .iter()
+            .map(|s| Ok(PathBuf::from(s.to_string()))),
+    ) {
         if let Ok(path) = entry {
             let path = Path::new(&path);
             if let Some(filename) = path.file_name().and_then(OsStr::to_str) {
                 if filename.ends_with(&version) {
                     let new_filename = &filename[..filename.len() - version.len()];
                     let new_path = path.with_file_name(new_filename);
-                    
+
+                    if let Ok(metadata) = symlink_metadata(&new_path) {
+                        if metadata.file_type().is_symlink() {
+                            if let Err(e) = remove_file(&new_path) {
+                                eprintln!(
+                                    "Failed to remove existing symlink {}: {}",
+                                    new_path.display(),
+                                    e
+                                );
+                                continue;
+                            }
+                        }
+                    }
+
                     println!("Linking {} to {}", path.display(), new_path.display());
                     if let Err(e) = fs::symlink(&path, &new_path) {
                         eprintln!("Failed to create symlink: {}", e);
